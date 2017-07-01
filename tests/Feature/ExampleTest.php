52ae9a149cc96135274f597ac801504cbb72a657
Vinay Aggarwal <vinayaggarwal@softway.com>
2017-07-01 13:18:28 +0530

    initial commit

---
+++ tests/Feature/ExampleTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicTest()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+
+    /** @test */
+    function an_unauthorizes_user_cannot_see_home()
+    {
+        $response = $this->get('/home');
+
+        $response->assertRedirect('/login');
+    }
+}
